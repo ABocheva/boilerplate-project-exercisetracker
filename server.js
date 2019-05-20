5a0cf70a746371a5be0b70666b9cd6daf53ece58
Antoniya Bocheva <abocheva65@googlemail.com>
2019-05-20 12:13:40 +0100

    more chnages to getting id nd username

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(function middleware(req, res, next) {
-  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
-  next();
+console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+next();
 });
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 var newUser = new mongoose.Schema({
-  "new_user": String,
-  "Id": String,
-  "log": [{ description: String, duration: Number, date: Date }]
+    "new_user": String,
+    "Id": String,
+    "log" : [{ description: String, duration: Number, date: Date }]
 });
 
 var createNewUser = mongoose.model("createNewUser", newUser);
@@ -23,40 +23,79 @@ var createNewUser = mongoose.model("createNewUser", newUser);
 
 app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/exercise/new-user', function (req, res, next) {
-  const username = req.body.username;
-  const userId = generateId(7);
+app.post('/api/exercise/new-user', function(req, res,next){
+const username = req.body.username;
+//const userId = generateUniqueId(7);   
+  createNewUser.findOne({ "new_user": username}, function (err, datauser) {
+//check if username exists first
+        if (datauser) {
+            res.send("This username is already taken")
+        } else {
+            var userId = GenerateUniqueId(7);
+//saving username and id in db in order to be able to get it after          
+            const newUsernameAndId = new createNewUser({
+              "new_user": username,
+              "Id": userId
+            });
 
+            newUsernameAndId.save();
 
-  res.json({ "new_user": username, "Id": userId });
+    res.json({"new_user": username, "Id": userId});
+        }
+        });
 });
 
-
-function generateId(length) {
-  var newId = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    newId += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return newId;
+//generate unique  Id
+function GenerateUniqueId(lengthOfId) {
+   var newId  = '';
+   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      newId += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return newId;
 };
+
+
+app.post('/api/exercise/add', function(req,res){
+const userId = req.body.userId;
+const description = req.body.description;
+const duration = req.body.duration;
+const date = req.body.date;
+const requiredFieldsToComplete = userId && description && duration;
+
+if(requiredFieldsToComplete == null){
+  res.send("Please complere required fields");
+} else{
+createNewUser.findById(userId, function(error, user){
+if(user){
+
+}
+});
+
+}
+
+
+
+});
+
+
 
 
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({ status: 404, message: 'not found' })
+  return next({status: 404, message: 'not found'})
 })
 
 // Error Handling middleware
@@ -81,4 +120,5 @@ app.use((err, req, res, next) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
 
