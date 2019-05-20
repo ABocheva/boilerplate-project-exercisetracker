925dd07f7544e56eb8d7be5741cc179529aae7e6
Antoniya Bocheva <abocheva65@googlemail.com>
2019-05-20 11:00:50 +0100

    first amendments
    added footer to html and css;
    post username and geerated id for each username

--- server.js
+++ server.js
@@ -4,24 +4,59 @@ const bodyParser = require('body-parser')
 
 const cors = require('cors')
 
+app.use(function middleware(req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next();
+});
+
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+var newUser = new mongoose.Schema({
+  "new_user": String,
+  "Id": String,
+  "log": [{ description: String, duration: Number, date: Date }]
+});
+
+var createNewUser = mongoose.model("createNewUser", newUser);
+
 
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/exercise/new-user', function (req, res, next) {
+  const username = req.body.username;
+  const userId = generateId(7);
+
+
+  res.json({ "new_user": username, "Id": userId });
+});
+
+
+function generateId(length) {
+  var newId = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    newId += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return newId;
+};
+
+
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+  return next({ status: 404, message: 'not found' })
 })
 
 // Error Handling middleware
@@ -46,3 +81,4 @@ app.use((err, req, res, next) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
